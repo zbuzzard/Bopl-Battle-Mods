c84bdd5f2f3d82ae65913e6e1102ac4c6ae984a9
zbuzzard <160368610+zbuzzard@users.noreply.github.com>
2024-02-19 01:56:09 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+namespace AntiSquish
+{
+    [BepInPlugin("com.buzzard.antisquish", "No Squish", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Plugin NoSquish is loaded!");
+
+            Harmony harmony = new Harmony("com.buzzard.antisquish");
+
+            MethodInfo grenadeoriginal = AccessTools.Method(typeof(PlayerCollision), "Awake");
+            MethodInfo grenadepatch = AccessTools.Method(typeof(myPatches), "Awake_AntiSquish_Plug");
+            harmony.Patch(grenadeoriginal, new HarmonyMethod(grenadepatch));
+
+        }
+
+        public class myPatches
+        {
+            public static void Awake_AntiSquish_Plug(PlayerCollision __instance)
+            {
+                __instance.isSquishable = false;
+
+            }
+        }
+    }
+}
